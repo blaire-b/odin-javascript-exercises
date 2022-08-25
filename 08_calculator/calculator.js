6af7f2d569d7be639dfe9ab51c732a1b69909f09
Blaire Belle <73305816+blaire-b@users.noreply.github.com>
2022-08-25 16:26:01 -0400

    calc completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let total = numbers.reduce(function(prev,curr) {
+    return curr + prev;
+  }, 0)
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+let newTotal = num.reduce(function(a, b) {
+  return a * b;
+})
+  return newTotal;
 };
 
-const power = function() {
-	
-};
+const power = function(a, b) {
+	    return a ** b;
+  };
 
-const factorial = function() {
-	
+
+const factorial = function(number) {
+	if (number === 0 || number === 1) {
+    return 1; }
+  for (let i = number - 1; i >= 1; i--) {
+    number *= i;
+  }
+  return number;
 };
 
 // Do not edit below this line
