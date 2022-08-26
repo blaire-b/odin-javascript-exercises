b377ef31349c57fd033bf6956a9540a756c542af
Blaire Belle <73305816+blaire-b@users.noreply.github.com>
2022-08-26 15:28:54 -0400

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        const lastGuy = age(oldest.yearOfBirth, oldest.yearOfDeath);
+        const nextGuy = age(current.yearOfBirth, current.yearOfDeath);
+        return lastGuy < nextGuy ? current : oldest;
+    })
 };
+const age = function(b, d) {
+    if (!d) {
+        d = new Date().getFullYear();
+    }
+    return d - b;
+};    
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
