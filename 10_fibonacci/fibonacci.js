c66625ade73e774ee59c7ad9c06bff4e7a3440f2
Blaire Belle <73305816+blaire-b@users.noreply.github.com>
2022-08-26 14:32:13 -0400

    completed fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
 
+let n=0;
+let n1=1;
+for (let i=1; i<number; i++) {
+    const next = n1;
+    n1 = n + n1;
+    n = next; 
+}
+return n1;
 };
 
 // Do not edit below this line
