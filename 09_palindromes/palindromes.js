6916f6d7c3d9324e56ddec864ee92eab98d31a9b
Blaire Belle <73305816+blaire-b@users.noreply.github.com>
2022-08-26 13:13:20 -0400

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+    newString = string.toLowerCase().replace(/[^a-z]/g, '');
+    return (newString.split('').reverse().join('') == newString); 
+}
 // Do not edit below this line
 module.exports = palindromes;
